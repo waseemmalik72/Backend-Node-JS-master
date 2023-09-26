a9811ec1953c3debe301699e23f4eb1867421cb8
waseemmalik72 <zmwaseem66@gmail.com>
2023-09-26 13:06:14 +0500

    some minor changes

--- 05.React.js/2. props/src/index.js
+++ 05.React.js/2. props/src/index.js
@@ -8,7 +8,7 @@ import {
   HandThumbsUp,
   ChatLeftText,
   ShareFill,
-  CodeSlash,
+  // CodeSlash,
 } from "react-bootstrap-icons";
 import "./App.css";
 
@@ -133,18 +133,63 @@ const Room = ({ myClass }) => {
 
 ReactDOM.render(<Room />, document.querySelector("#root"));
 
+const MyComponent = () => {
+  const [isLit, setLit] = useState("nadeem");
+
+  useEffect(() => {
+    console.log("hello world");
+  }, [isLit]);
+
+  const submitHandler = () => {
+    setLit((prev) => `${prev} waseem`);
+  };
+
+  return (
+    <div className="main-div">
+      <button className="btn" onClick={submitHandler}>
+        Click me
+      </button>
+      <h1>{isLit}</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<MyComponent />, document.querySelector("#root"));
+
+const WeatherCard = ({ item }) => {
+  return (
+    <div className="my-data">
+      {item.data.map((data, index) => {
+        return (
+          <div className="weather-card" key={index}>
+            <h1>{data.city_name}</h1>
+            <h2>{data.weather.description}</h2>
+            <h3>{data.temp}°C</h3>
+            <h4>{data.datetime}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 const Weather = () => {
   const inputRef = useRef();
+  const [myWeather, setWeather] = useState([]);
   const Api_KEY = "a26dc0b7cab148668f6744215b44550c";
+
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(async (location) => {
         console.log(location.coords.latitude, location.coords.longitude);
         try {
-          let respnse = await axios.get(
-            `https://api.weatherbit.io/v2.0/current?lat=${location.coords.latitude}&lon=${location.coords.longitude}&key=${Api_KEY}&units=I`
+          let response = await axios.get(
+            `https://api.weatherbit.io/v2.0/current?lat=${location.coords.latitude}&lon=${location.coords.longitude}&key=${Api_KEY}`
           );
-          console.log(respnse.data);
+          // setWeather([response.data, ...myWeather]);
+          setWeather((prev) => {
+            return [response.data, ...prev];
+          });
         } catch (error) {
           console.log(error);
         }
@@ -153,17 +198,28 @@ const Weather = () => {
       console.log("Geolocation is not supported by this browser.");
     }
   }, []);
-  const submitHandler = (e) => {
+
+  const submitHandler = async (e) => {
     e.preventDefault();
+    try {
+      let response = await axios.get(
+        `https://api.weatherbit.io/v2.0/current?&key=${Api_KEY}&&city=${inputRef.current.value}`
+      );
+      setWeather((prev) => [response.data, ...prev]);
+      console.log(myWeather);
+      inputRef.current.value = "";
+    } catch (error) {
+      console.log(error);
+    }
     // console.log("hello world");
   };
+
   return (
     <div className="callApi-form">
       <form action="" onSubmit={submitHandler}>
         <label htmlFor="callApi">Search Weather</label>
         <input
           type="text"
-          name=""
           id="callApi"
           ref={inputRef}
           required
@@ -174,7 +230,18 @@ const Weather = () => {
           Get Data
         </button>
       </form>
-      <div className="card"></div>
+      {!myWeather.length ? (
+        <h1>loading...</h1>
+      ) : myWeather.length === 0 ? (
+        <h1> No Data</h1>
+      ) : null}
+      <div className="card">
+        {myWeather.length
+          ? myWeather.map((item, index) => {
+              return <WeatherCard item={item} key={index} />;
+            })
+          : null}
+      </div>
     </div>
   );
 };
